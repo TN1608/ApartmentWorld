63d40e1105dad4dcfebb6f0ab4ed5006c50b74d2
TN1608 <120995414+TN1608@users.noreply.github.com>
2024-10-04 12:27:46 +0700

    DANG LAM CCCD

--- src/main/java/java5/asm/dao/CCCDDao.java
+++ src/main/java/java5/asm/dao/CCCDDao.java
@@ -4,5 +4,4 @@ import java5.asm.model.CCCD;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CCCDDao extends JpaRepository<CCCD,String> {
-    <Optional> CCCD findByTentaikhoan(String tentaikhoan);
 }
